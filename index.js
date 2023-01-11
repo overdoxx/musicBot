7ec39229a9b80312a5fad3153b4b0767ac7dfafe
Bonës <gustavomontiselli@gmail.com>
2023-01-11 02:40:48 -0300

    a

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ require("./src/utils/Types")();
 const Bot = require("./src/struct/Bot");
 const client = new Bot();
 
+// AntiCrash
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught Exception: " + err);
+});
+  
+process.on("unhandledRejection", (reason, promise) => {
+    console.log("[GRAVE] Rejeição possivelmente não tratada em: Promise ", promise, " motivo: ", reason.message);
+});
 (async () => await client.start(process.env.TOKEN))();
